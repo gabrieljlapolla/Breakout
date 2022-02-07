6baeaf08c745dda1fd8b44bc1d674543a9c26aec
Gabriel Lapolla <gabrieljlapolla@protonmail.com>
2022-02-06 21:50:29 -0500

    Rounded paddle

--- src/Paddle.java
+++ src/Paddle.java
@@ -3,6 +3,7 @@
  */
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import javax.swing.BorderFactory;
 import javax.swing.JComponent;
 import javax.swing.border.Border;
@@ -19,15 +20,10 @@ public class Paddle extends JComponent {
     }
     
     protected void paintComponent(Graphics g) {
-        g.setColor(new Color(92, 22, 78));
-        g.fillRect(0, 0, getSize().width - 1, getSize().height - 1);
-        super.paintComponent(g);
-        // TODO: Add rounded corners
-    }
-    
-    protected void paintBorder(Graphics g) {
-        g.setColor(new Color(155, 40, 123));
-        g.drawRect(0,0, getSize().width - 1, getSize().height - 1);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(new Color(92, 22, 78));
+        g2d.fillRoundRect(0, 0, getSize().width - 1, getSize().height - 1, getSize().height, getSize().height);
+        super.paintComponent(g2d);
     }
     
     public void initPaddle(int windowWidth, int windowHeight) {
